45c65a19f978a728e68c3da85eef21948b45e46d
Jess Cushnie <94955791+jmcushnie@users.noreply.github.com>
2023-05-08 19:18:44 +1200

    Update CardList.js

--- src/CardList.js
+++ src/CardList.js
@@ -1,14 +1,30 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import Card from "./components/Card";
 
 const CardList = () => {
+  const [cocktails, setCocktails] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await axios.get(
+        "https://www.thecocktaildb.com/api/json/v1/1/random.php"
+      );
+      const newCocktails = Array.from(
+        { length: 20 },
+        () => response.data.drinks[0]
+      );
+      setCocktails(newCocktails);
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <div className="cocktail-list">
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
+      {cocktails.map((cocktail, index) => (
+        <Card key={index} cocktail={cocktail} />
+      ))}
     </div>
   );
 };
