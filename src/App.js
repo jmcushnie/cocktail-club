cd7c30bdb4bb654802f1b397963d55d8553890c2
Jess Cushnie <94955791+jmcushnie@users.noreply.github.com>
2023-05-08 19:46:00 +1200

    Search box and styling
    Created searchbox component and css styling

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from "react";
 import Card from "./components/Card";
-import { useState, useCallback } from "react";
 import CardList from "./CardList";
+import SearchBox from "./components/SearchBox";
 
 function App() {
   return (
     <div className="App">
       <h1>Cocktail club</h1>
       <div>
+        <SearchBox />
         <CardList />
       </div>
     </div>
---
+++ src/components/SearchBox.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const SearchBox = () => {
+  return (
+    <div className="search-box-container">
+      <input
+        type="search"
+        placeholder="Search Cocktails by Name or Ingredients..."
+      />
+    </div>
+  );
+};
+
+export default SearchBox;
