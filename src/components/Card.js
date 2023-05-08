fe165b2917a5739a60b0b48025be4a0997d5dcd3
Jess Cushnie <94955791+jmcushnie@users.noreply.github.com>
2023-05-08 17:15:51 +1200

    initial commit

---
+++ src/components/Card.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Card = () => {
+  return (
+    <div className="cocktail-card">
+      Card
+      <img alt="cocktail photo" src="" />
+      <div>
+        <h2>Name</h2>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
