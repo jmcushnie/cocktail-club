6483f368b27ff87a3dd145e993ecb582449556d7
Jess Cushnie <94955791+jmcushnie@users.noreply.github.com>
2023-05-11 11:23:01 +1200

    cocktail details styling
    css

--- src/components/CocktailDetails.js
+++ src/components/CocktailDetails.js
@@ -1,17 +1,37 @@
 import React from "react";
 
-const CocktailDetails = ({ cocktail }) => {
+const CocktailDetails = ({ cocktail, searchValue }) => {
   if (!cocktail) {
-    return <div>Loading...</div>;
+    return <div>No Cockatil found</div>;
   }
 
   return (
     <div className="cocktail-details">
+      <img
+        alt="cocktail"
+        src={cocktail.strDrinkThumb}
+        style={{ width: "300px", height: "300px" }}
+      />
       <h2>{cocktail.strDrink}</h2>
-      <p>
-        Ingredients: {cocktail.strIngredient1}, {cocktail.strIngredient2}, ...
-      </p>
-      <p>Instructions: {cocktail.strInstructions}</p>
+      <h3 className="Ingredients">Ingredients:</h3>
+      <ul>
+        <li>
+          {cocktail.strMeasure1} {cocktail.strIngredient1}
+        </li>
+        <li>
+          {cocktail.strMeasure2} {cocktail.strIngredient2}
+        </li>
+        <li>
+          {cocktail.strMeasure3} {cocktail.strIngredient3}
+        </li>{" "}
+        <li>
+          {cocktail.strMeasure4} {cocktail.strIngredient4}
+        </li>{" "}
+        <li>
+          {cocktail.strMeasure5} {cocktail.strIngredient5}
+        </li>
+      </ul>
+      <h3>Instructions:</h3> <p>{cocktail.strInstructions}</p>
     </div>
   );
 };
